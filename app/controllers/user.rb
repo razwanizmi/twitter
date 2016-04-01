cb9a3f0857f7d617b87685654cdea2cadefd0cb3
razwanizmi <razwan.izmi@gmail.com>
2016-04-01 11:52:27 +0800

    Added edit user feature. Fixed user model to only validate password on creation.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -16,3 +16,13 @@ get '/users/:id' do
   @user = User.find(params[:id])
   erb :"user/profile"
 end
+
+get '/users/:id/edit' do
+  erb :"user/edit_profile"
+end
+
+put '/users/:id' do
+  user = User.find(params[:id])
+  user.update(params[:user])
+  redirect "/users/#{params[:id]}"
+end
