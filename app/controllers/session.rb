46e7179ecc4772d384a8e3fb1173168f9e3e0ac3
razwanizmi <razwan.izmi@gmail.com>
2016-04-01 11:11:39 +0800

    Added user login features.

---
+++ app/controllers/session.rb
@@ -0,0 +1,14 @@
+get '/sessions/new' do
+  erb :"session/login"
+end
+
+post '/sessions' do
+  user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
+  if user
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    @message = "Error loggin in. Please ensure username and password are correct."
+    erb :"session/login"
+  end
+end
