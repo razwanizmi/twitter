549cc7d0b6f549dc4f8507803f13402d36e21e51
razwanizmi <razwan.izmi@gmail.com>
2016-04-01 13:01:47 +0800

    Added new tweet function.

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -1,3 +1,13 @@
 get '/tweets/new' do
   erb :"tweet/new_tweet"  
 end
+
+post '/tweets' do
+  tweet = Tweet.new(params[:tweet])
+  if tweet.save
+    redirect '/'
+  else
+    @error = tweet.errors.full_messages.first
+    erb :"tweet/new_tweet"
+  end
+end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
-class User < ActiveRecord::Base
+class Tweet < ActiveRecord::Base
   validates_length_of :description, maximum: 140, message: "Tweets must not exceed 140 characters", on: :create
   belongs_to :user
 end
